8ae2bf7fd02ea1648c9d370f1b0fe5ec417b5923
Hardik <hardikgw@gmail.com>
2020-02-26 11:03:34 -0500

    filter

--- src/main/java/biz/cits/reactive/model/MessageRepo.java
+++ src/main/java/biz/cits/reactive/model/MessageRepo.java
@@ -10,7 +10,10 @@ import java.util.stream.Stream;
 @Repository
 public class MessageRepo {
     public Flux<Message> getMessages(String filter) {
-        return Flux.fromStream(Stream.generate(() -> new Message(MsgGenerator.getMessages(1).get(0).getValue())))
+        return Flux.fromStream(Stream.generate(() -> new Message(MsgGenerator.getMessages(1).get(0).getValue())).filter(message -> {
+            System.out.println("filtering --> " + message.getMessage());
+            return message.getMessage().startsWith(filter);
+        }))
                 .delayElements(Duration.ofMillis(100));
     }
 }
--- src/main/java/biz/cits/reactive/rsocket/RSocketController.java
+++ src/main/java/biz/cits/reactive/rsocket/RSocketController.java
@@ -3,6 +3,7 @@ package biz.cits.reactive.rsocket;
 import biz.cits.reactive.model.Message;
 import biz.cits.reactive.model.MessageRepo;
 import org.reactivestreams.Publisher;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
@@ -16,9 +17,10 @@ public class RSocketController {
         this.messageRepo = messageRepo;
     }
 
-    @MessageMapping("messages")
-    public Publisher<Message> feedMarketData() {
-        return messageRepo.getMessages("world");
+    @MessageMapping("messages/{filter}")
+    public Publisher<Message> getMessages(@DestinationVariable String filter) {
+        System.out.println("FILTER -----> " + filter);
+        return messageRepo.getMessages(filter);
     }
 
 }
