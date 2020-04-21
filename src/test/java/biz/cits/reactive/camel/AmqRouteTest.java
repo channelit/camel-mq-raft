9dc29b69f98bca4bb67b0c5af6511e2cdc6a1038
Hardik <hardikgw@gmail.com>
2020-04-21 03:58:44 -0400

    unit test setup

--- src/test/java/biz/cits/reactive/camel/AmqRouteTest.java
+++ src/test/java/biz/cits/reactive/camel/AmqRouteTest.java
@@ -1,48 +1,94 @@
 package biz.cits.reactive.camel;
 
+import biz.cits.reactive.message.MsgGenerator;
+import biz.cits.reactive.model.ClientMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.component.jms.JmsEndpoint;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.component.reactive.streams.api.CamelReactiveStreamsService;
 import org.apache.camel.test.spring.CamelSpringRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.TestPropertySource;
+import reactor.core.publisher.Flux;
+
+import javax.jms.TextMessage;
+import java.time.Instant;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 
 @RunWith(CamelSpringRunner.class)
 @SpringBootTest
 @TestPropertySource(properties = {"spring.activemq.broker-url=vm://localhost?broker.persistent=false"})
 public class AmqRouteTest {
 
+    private ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule()).configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
     @Autowired
     protected CamelContext camelContext;
 
+    @Autowired
+    protected CamelReactiveStreamsService camel;
+
     protected MockEndpoint mockB;
 
-    protected JmsEndpoint endpoint;
+    @Autowired
+    protected JmsTemplate jmsTemplate;
 
     @EndpointInject(value = "mock:c")
     protected MockEndpoint mockC;
 
     @Produce("jms:queue:in-topic")
-    protected ProducerTemplate start2;
+    protected ProducerTemplate start;
 
-    @EndpointInject(value = "mock:log:biz.cits.reactive")
-    protected MockEndpoint mockLog;
+    @Produce("jms:topic:VirtualTopic.out-topic")
+    protected ProducerTemplate end;
+
+    private String inTopic = "in-topic";
 
     @Test
     public void testPositive() throws Exception {
 
-        mockC.expectedBodiesReceived("David");
-        mockLog.expectedBodiesReceived("Hello David");
+        Map.Entry<String, String> message = MsgGenerator.getMessages(1).get(0);
 
-        start2.sendBody("David");
+        ClientMessage clientMessage = ClientMessage.builder()
+                .client(message.getKey())
+                .id(UUID.randomUUID())
+                .content(message.getValue())
+                .messageDateTime(Instant.now()).build();
 
+        String jsonString = "";
+        try {
+            jsonString = mapper.writeValueAsString(clientMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        final String messageString = jsonString;
+        jmsTemplate.send(new ActiveMQQueue(inTopic), messageCreator -> {
+            TextMessage textMessage = messageCreator.createTextMessage(messageString);
+            textMessage.setStringProperty("client", "client");
+            return textMessage;
+        });
+        CountDownLatch receptionLatch = new CountDownLatch(1);
         MockEndpoint.assertIsSatisfied(camelContext);
+        Flux.from(camel.from("reactive-streams:message_out_stream")).subscribe(
+                m -> {
+                    receptionLatch.countDown();
+                }
+        );
+
     }
 
 
